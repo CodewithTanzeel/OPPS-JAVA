e029f5a5d44f8d5306d236e9f4ecc248a4ba47d8
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-03-07 23:58:52 +0500

    ,.

---
+++ HelloWorld/helloworld.java
@@ -0,0 +1,5 @@
+public class HelloWorld{
+    public static void main(strg[] msg){
+        system.out println("Hello World!!!")
+    }
+}
---
+++ areaofcircle/AreaofCircle/aoc.java
@@ -0,0 +1,12 @@
+public class Welcome{
+    public static void main(string[] args){
+        double area;
+        double radius;
+
+        area = radius*radius*3.14297
+    
+        System.out.println("Area of Circle is :"+area)
+
+
+    }
+}
