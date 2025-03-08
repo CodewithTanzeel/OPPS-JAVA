a0928008567771084fcb0b52dc417898cfdc9c49
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-03-09 00:31:42 +0500

    >

---
+++ BooleanTest/BooleanTest.java
@@ -0,0 +1,10 @@
+public class BooleanTest{
+    public static void main(String[] args){
+        int x = 1;
+        int y = x-1;
+        boolean test = x > y;
+        boolean test2 = y > x;
+        System.out.println("X is greater then Y : "+test);
+        System.out.println("X is greater then Y : "+test2);
+    }
+}
---
+++ CastingConvertion/CastingConvertion.java
@@ -0,0 +1,8 @@
+public class CastingConvertion{
+    public static void main(String[] args){
+        boolean b = true;
+        i = (int)b;
+        // java doesnt allow different type conversions!!!
+        System.out.println(i);
+    }
+}
