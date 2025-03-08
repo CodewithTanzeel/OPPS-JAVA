dd5c1079d74e3e4a53b1998440bf92bb789ba019
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-03-09 00:52:33 +0500

    >

---
+++ Conditionals/Conditionals.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Conditionals{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any integer :");
+        int x  = input.nextInt();
+        if (x>0){
+            System.out.println("Entered Output is Positive");
+        }
+        
+    }
+}
