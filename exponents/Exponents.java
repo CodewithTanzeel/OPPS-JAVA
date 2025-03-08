99a8cd5ee041dedd001a833a979f49fe615ff281
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-03-08 12:59:09 +0500

    >

---
+++ exponents/Exponents.java
@@ -0,0 +1,12 @@
+public class Exponents{
+    public static void main(String args[]){
+        System.out.println(Math.pow(2,3)
+        );
+        System.out.println(Math.pow(4,0.5)
+        );
+        System.out.println(Math.pow(2.5,2)
+        );
+        System.out.println(Math.pow(2.5,-2)
+        );
+    }
+}
