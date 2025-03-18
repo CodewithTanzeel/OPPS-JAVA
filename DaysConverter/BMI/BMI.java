a2ba830c628062d8909c8f67d5ac22a824297a41
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-03-18 09:05:57 +0500

    .

---
+++ DaysConverter/BMI/BMI.java
@@ -0,0 +1,13 @@
+ import java.util.Scanner;
+public class BMI{
+    public static void main(String[] args){
+        Scanner input  = new Scanner(System.in);
+        System.out.print("Enter no 1 : ");
+        int No1 = input.nextInt();
+        System.out.print("Enter no 2 : ");
+        int No2 = input.nextInt();
+        double sum = No1 + No2;
+        System.out.println("The sum is "+ sum );
+
+    }
+}
