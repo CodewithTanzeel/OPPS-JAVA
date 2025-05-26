d0e17c16105242cfeb80e2e548b0be379932ef70
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-05-26 23:33:26 +0500

    .

---
+++ DimensionalPolymorphism/src/dimensionalpolymorphism/DimensionalPolymorphism.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dimensionalpolymorphism;
+
+
+public class DimensionalPolymorphism {
+   double dim1;
+   Figure(){
+       this.dim1 = -1;
+       
+   }
+   Figure(double dim1){
+       this.dim1 = dim1;
+       
+   }
+   void area(){
+       System.out.println("area is undefined");
+      
+       
+   }
+   public String toString(){
+      return "Dim1 = "+this.dim1;
+       
+   }
+    
+}
