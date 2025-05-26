9bef44526eb04be98711eaf4ccf310c2402d33e0
TanzeelAhmad <Tanzeelofficial@outlook.com>
2025-05-26 11:02:12 +0500

    .

--- TryInitialization/src/tryinitialization/TryInitialization.java
+++ TryInitialization/src/tryinitialization/TryInitialization.java
@@ -8,13 +8,39 @@ package tryinitialization;
  *
  * @author FATTANI COMPUTERS
  */
-public class TryInitialization {
 
-    /**
-     * @param args the command line arguments
-     */
+public class TryInitialization {
+    static int[] values = new int[10];
+    static{
+        System.out.println("Running initialization block.");
+        for(int i = 0; i < values.length; i++){
+            values[i] = (int)(100.0 * Math.random());
+            
+        }
+        
+    }
+
+    //List values in the arry of an object;
+    
+    
+    void listValues(){
+        //AlternativeApproach  make this method static
+        System.out.println();
+        for (int value:values){
+            System.out.println(" "+ value);
+        }
+        System.out.println();
+        }
     public static void main(String[] args) {
-        // TODO code application logic here
+        TryInitialization example  = new TryInitialization();
+        System.out.println("\n FirstObject:");
+        example.listValues();
+        
+        
+        example = new TryInitialization();
+        System.out.println("\n Second object");
+        example.listValues();
+        // since main is static that why we have to call it through an object
     }
     
 }
